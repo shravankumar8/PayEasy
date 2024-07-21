b2362ba788c6a6c335c22ba994f9c577defd7455
shravan kumar <kumashravan5@gmail.com>
2024-07-21 12:23:25 +0530

    added tailwind css

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div> 
+    <div className="">
+      hii
+    </div>
+   </div>
+  );
+}
